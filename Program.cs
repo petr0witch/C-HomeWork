6acc1b4c348113e072ffa94b9fd71139ba444952
Petro_witch <wonkewood@gmail.com>
2022-12-18 17:09:35 +0300

    Ex_4

--- Program.cs
+++ Program.cs
@@ -62,3 +62,22 @@ if (number % 2 == 0) {
     Console.WriteLine($"Число {number} - нечётное");
 }
 */
+
+/*Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while (index <= number) {
+    if (index % 2 == 0){
+        Console.Write(index + " ");
+        index++;
+    }
+    else {
+        index++;
+    }
+}
